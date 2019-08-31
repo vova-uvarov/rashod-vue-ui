d4565d1f6cca54b7e703739b2598dbbf5a053e16
vova.uvarov <vova.uvarov@bssys.com>
2019-08-31 18:55:23 +0300

    simple structure for rashod components

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Главная страница</router-link> |
+      <router-link to="/accounts">Счета</router-link>|
+      <router-link to="/operations">Операции</router-link>|
+      <router-link to="/statistics">Статистика</router-link>
     </div>
     <router-view/>
   </div>
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import Main from './views/Main.vue';
 
 Vue.use(Router);
 
@@ -8,16 +8,32 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'main',
+      component: Main,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/accounts',
+      name: 'accounts',
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+      component: () => import(/* webpackChunkName: "about" */ './views/Accounts.vue'),
+    },
+    {
+      path: '/operations',
+      name: 'operations',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Operations.vue'),
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Statistics.vue'),
     },
   ],
 });
