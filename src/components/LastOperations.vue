a18ee6761d99df4fc06f10f17922b18cf15ce2f3
vova-uvarov <vova.uvarov@gmail.com>
2019-09-05 23:52:40 +0300

    Набросок редактирования операций. Пока как-то все сложно

--- src/components/LastOperations.vue
+++ src/components/LastOperations.vue
@@ -2,7 +2,7 @@
     <v-row justify="center" align="start">
         <v-col cols="12">
             <v-card :color="cardColor">
-                <v-card-title>{{title}}</v-card-title>
+                <v-card-title>{{title}} = {{showEditDialog}}</v-card-title>
                 <v-simple-table>
                     <thead>
                     <tr>
@@ -10,6 +10,7 @@
                         <th class="text-left">Категория</th>
                         <th class="text-left">Список</th>
                         <th class="text-left">Сумма</th>
+                        <th class="text-left"></th>
                     </tr>
                     </thead>
                     <tbody>
@@ -18,18 +19,27 @@
                         <td>{{ item.category.name }}</td>
                         <td>{{ shoppingListFormatter(item.shoppingList)|truncateString }}</td>
                         <td>{{ item.cost |moneyFormat}}</td>
+                        <td>
+                            <v-btn class="mx-2" fab dark small color="primary" @click.stop="editOperation(item.id)">
+                                <v-icon>mdi-file-document-edit</v-icon>
+                            </v-btn>
+                        </td>
                     </tr>
                     </tbody>
                 </v-simple-table>
             </v-card>
         </v-col>
+        <EditOperationDialog :operationId="operationId" :visible="showEditDialog" @close="showEditDialog=false"/>
     </v-row>
 </template>
 
 <script lang="ts">
     import {Component, Prop, Vue} from "vue-property-decorator";
+    import EditOperationDialog from "@/components/EditOperationDialog.vue";
 
-    @Component
+    @Component({
+        components: {EditOperationDialog}
+    })
     export default class AccountList extends Vue {
         @Prop({default: []})
         operations: Array<Object>;
@@ -40,10 +50,23 @@
         @Prop({default: "white"})
         cardColor: string;
 
+        editOperation(selectOperationId) {
+            this.operationId = selectOperationId;
+            this.showEditDialog = true;
+        }
+
+
+        data() {
+            return {
+                operationId: null,
+                showEditDialog: false
+            };
+        }
+
         //todo Копипаста из LastOpeartions
-        shoppingListFormatter(shoppingList){
-            if (!shoppingList){
-                return '';
+        shoppingListFormatter(shoppingList) {
+            if (!shoppingList) {
+                return "";
             }
             return shoppingList.map(val => (val.name)).toString();
         }
