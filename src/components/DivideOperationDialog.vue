f5fe3ec88aff1a20a34e336301fc64f164b5465d
uvarovvy <uvarov.vy@reliab.tech>
2019-09-06 15:03:51 +0300

    Заготовка для разбивания операции

---
+++ src/components/DivideOperationDialog.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-row justify="center">
+
+        <v-dialog
+                max-width="790"
+                v-model="showDialog"
+        >
+            <ShortOperationInfoBar :operation="operation"/>
+            <CreateOperation formMode="DIVIDE" :operation="operation" v-if="!loading"/>
+            <span v-else>Идет загрузка...</span>
+        </v-dialog>
+    </v-row>
+</template>
+<script lang="ts">
+
+    import {Component, Prop, Vue, Watch} from "vue-property-decorator";
+    import CreateOperation from "@/components/CreateOperation.vue";
+    import ShortOperationInfoBar from "@/components/ShortOperationInfoBar.vue";
+    import OperationService from "@/services/OperationService";
+
+    @Component({
+        components: {CreateOperation, ShortOperationInfoBar}
+    })
+    export default class DivideOperationDialog extends Vue {
+
+        @Prop()
+        parentOperationId: string;
+
+        @Prop()
+        visible: string;
+
+        get showDialog() {
+            return this.visible;
+        }
+
+        set showDialog(value) {
+            if (!value) {
+                this.$emit("close");
+            }
+        }
+
+        @Watch("parentOperationId")
+        operationIdChanged(value: string, oldValue: string) {
+            this.loading = true;
+            OperationService.get(value)
+                .then((data) => {
+                        this.loading = false;
+                        this.operation = data;
+                        this.operation.id = null;
+                        this.operation.parentOperationId = value;
+                    }
+                )
+        }
+
+        data() {
+            return {
+                loading: false,
+                operation: {}
+            };
+        }
+    }
+</script>
---
+++ src/components/ShortOperationInfoBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-toolbar>
+        Категория: {{operation.category.name}}, Сумма: {{operation.cost|moneyFormat}}, Место: {{operation.place}}
+    </v-toolbar>
+</template>
+<script lang="ts">
+
+    import {Component, Prop, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class ShowOperationInfoBar extends Vue {
+        @Prop()
+        operation: object;
+    }
+</script>
