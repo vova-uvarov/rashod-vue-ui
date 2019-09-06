9a73c6706779a3159dbb0591a66cc8f9be6c88a5
uvarovvy <uvarov.vy@reliab.tech>
2019-09-06 12:37:58 +0300

    remove date hacks

--- src/services/AccountService.ts
+++ src/services/AccountService.ts
@@ -17,25 +17,19 @@ export default class AccountService {
     public static loadBalances(): Promise<AxiosResponse<Balance>> {
         return axios
             .get('http://localhost:8092/api/accounts/balances')
-            .then((response) => { // todo этот код повторяется тут 3 раза
-                return response.data;
-            });
+            .then((response) => (response.data))
     }
 
     public static totalBalance(): Promise<AxiosResponse<any>> {
         return axios
             .get('http://localhost:8092/api/accounts/totalBalance')
-            .then((response) => {
-                return response.data;
-            });
+            .then((response) => (response.data))
     }
 
     public static balancesByCurrency(): Promise<AxiosResponse<any>> {
         return axios
             .get('http://localhost:8092/api/accounts/balances/goal/byCurrency')
-            .then((response) => {
-                return response.data;
-            });
+            .then((response) => (response.data));
     }
 
 }
