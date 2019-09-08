4cf90cca6dd8b655e81f728f0423a131842ab9a6
vova-uvarov <vova.uvarov@gmail.com>
2019-09-08 14:18:25 +0300

    divide plans by tabs

--- src/views/Plans.vue
+++ src/views/Plans.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="plans">
-        <OperationsList :operations="operations"/>
+        <v-tabs
+                v-model="tab"
+                align-with-title
+                background-color="transparent"
+        >
+            <v-tabs-slider color="yellow"></v-tabs-slider>
+
+            <v-tab v-for="item in itemTabs" :key="item">
+                {{ item }}
+            </v-tab>
+        </v-tabs>
+        <OperationsList :operations="operations(itemTabs[tab])"/>
     </div>
 </template>
 
 <script lang="ts">
-    import { Component, Vue } from 'vue-property-decorator';
-    import OperationsList  from '../components/OperationsList.vue'; // @ is an alias to /src
+    import {Component, Vue} from "vue-property-decorator";
+    import OperationsList from "../components/OperationsList.vue"; // @ is an alias to /src
 
     @Component({
         components: {
@@ -14,17 +25,30 @@
         }
     })
     export default class PlansView extends Vue {
-        created(){
+        created() {
             this.$store.dispatch("loadPlans");
         }
-        get operations() {
-            return this.$store.state.operationPlans.operations;
+
+        operations(categoryName: string) {
+            console.log("categoryName=" + categoryName);
+            return this.$store.state.operationPlans.operations.filter((op)=>(op.category.name==categoryName));
+        }
+
+        get itemTabs() {
+            let categoryNames = this.$store.state.operationPlans.operations.map((op) => (op.category.name));
+            return Array.from(new Set(categoryNames)); // this is distinct
         }
 
         mounted() {
             this.$root.$on("operationDeleted", () => {
                 this.$store.dispatch("loadPlans");
             });
+        }
+
+        data() {
+            return {
+                tab: null
+            };
         }
     }
 </script>
