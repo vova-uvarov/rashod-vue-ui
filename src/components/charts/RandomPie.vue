a9eb05b677e02b3eaee507ba2477e4a6e7def96a
vova.uvarov <vova.uvarov@bssys.com>
2019-09-09 00:32:59 +0300

    chartjs add two random charts for test

---
+++ src/components/charts/RandomPie.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="small">
+        <PieChart :chart-data="datacollection"></PieChart>
+        <button @click="fillData()">Randomize</button>
+    </div>
+</template>
+
+<script>
+    import PieChart from './PieChart.js'
+
+    export default {
+        components: {
+            PieChart
+        },
+        data() {
+            return {
+                datacollection: null
+            }
+        },
+        mounted() {
+            this.fillData()
+        },
+        methods: {
+            fillData() {
+                this.datacollection = {
+                    datasets: [{ backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+                        data: [this.getRandomInt(), 20, this.getRandomInt()]
+                    }],
+
+
+
+                    // These labels appear in the legend and in the tooltips when hovering different arcs
+                    labels: [
+                        'Red',
+                        'Yellow',
+                        'Blue'
+                    ]
+
+                }
+            },
+            getRandomInt() {
+                return Math.floor(Math.random() * (150 - 5 + 1)) + 5
+            }
+        }
+    }
+</script>
+
+<style>
+    .small {
+        max-width: 600px;
+        margin: 150px auto;
+    }
+</style>
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>Статистика</h1>
+    <v-container>
+        <h1>Статистика</h1>
+        <RandomPie/>
+        <RandomChart/>
+    </v-container>
 </template>
+<script lang="ts">
+    import RandomChart from "../components/charts/RandomChart.vue";
+    import RandomPie from "../components/charts/RandomPie.vue";
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component({
+        components: {
+            RandomChart,RandomPie
+        }
+    })
+    export default class StatisticsVue extends Vue {
+    }
+</script>
