268ce4c694610b8ea0e14c8c6605a4c243fb5c1b
uvarovvy <uvarov.vy@reliab.tech>
2019-09-10 11:24:00 +0300

    Доход/Расход по месяцам с выбором даты и исключающих категорий

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -6,17 +6,80 @@
                     <AccountBalancesPie/>
                 </v-card>
             </v-col>
-            <v-col cols="8">
-                <v-card>
-                    <IncomAndConsumptionByMonth :raw-data="incomeConsumptionByLastYear" title="Доход/Расход по месяцам в текущем году"/>
-                </v-card>
-            </v-col>
+            <!--            <v-col cols="8">-->
+            <!--                <v-card>-->
+            <!--                    <IncomAndConsumptionByMonth :raw-data="incomeConsumptionByLastYear"-->
+            <!--                                                title="Доход/Расход по месяцам в текущем году"/>-->
+            <!--                </v-card>-->
+            <!--            </v-col>-->
 
         </v-row>
         <v-row>
+            <v-row>
+                <v-col cols="4">
+                    <!--                todo нужно сделать компонент даты-->
+                    <v-menu
+                            v-model="dateFromMenu"
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            transition="scale-transition"
+                            offset-y
+                            full-width
+                            min-width="290px"
+                    >
+                        <template v-slot:activator="{ on }">
+                            <v-text-field
+                                    v-model="dateFrom"
+                                    label="Дата C"
+                                    prepend-icon="event"
+                                    readonly
+                                    v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="dateFrom"
+                                       @input="dateFromMenu = false"></v-date-picker>
+                    </v-menu>
+                </v-col>
+                <v-col cols="4">
+                    <!--                todo нужно сделать компонент даты-->
+                    <v-menu
+                            v-model="dateToMenu"
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            transition="scale-transition"
+                            offset-y
+                            full-width
+                            min-width="290px"
+                    >
+                        <template v-slot:activator="{ on }">
+                            <v-text-field
+                                    v-model="dateTo"
+                                    label="Дата По"
+                                    prepend-icon="event"
+                                    readonly
+                                    v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="dateTo"
+                                       @input="dateToMenu = false"></v-date-picker>
+                    </v-menu>
+                </v-col>
+                <v-col cols="4">
+                    <v-combobox
+                            :items="categories"
+                            v-model="excludeCategoryIds"
+                            :clearable="true"
+                            multiple
+                            :search-input.sync="searchCategoryValue"
+                            @change="searchCategoryValue = ''"
+                            label="Исключить категории"
+                    ></v-combobox>
+                </v-col>
+            </v-row>
             <v-col cols="12">
                 <v-card>
-                    <IncomAndConsumptionByMonth :raw-data="incomeConsumptionPrevious" title="Доход/Расход по месяцам в прошлом"/>
+                    <IncomAndConsumptionByMonth :raw-data="incomeConsumptionByMonth"
+                                                title="Доход/Расход по месяцам"/>
                 </v-card>
             </v-col>
         </v-row>
@@ -27,7 +90,7 @@
 <script lang="ts">
     import IncomAndConsumptionByMonth from "../components/charts/IncomAndConsumptionByMonth.vue";
     import AccountBalancesPie from "../components/charts/AccountBalancesPie.vue";
-    import {Component, Vue} from "vue-property-decorator";
+    import {Component, Vue, Watch} from "vue-property-decorator";
     import StatisticsService from '@/services/StatisticsService';
 
     @Component({
@@ -37,21 +100,52 @@
     })
     export default class StatisticsVue extends Vue {
         mounted() {
-            StatisticsService.incomeConsumptionByMonthLastYear()
-                .then((responseData) => {
-                    this.incomeConsumptionByLastYear = responseData;
-                });
+            this.loadData();
+        }
 
-            StatisticsService.incomeConsumptionByMonthLastPrevious()
-                .then((responseData) => {
-                    this.incomeConsumptionPrevious = responseData;
-                });
+        @Watch("dateFrom")
+        dateFromChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        @Watch("dateTo")
+        dateToChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        @Watch("excludeCategoryIds")
+        excludeCategoryIdsChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        private loadData() {
+            StatisticsService.incomeConsumptionByMonth(
+                this.dateFrom,
+                this.dateTo,
+                this.excludeCategoryIds
+            ).then((responseData) => {
+                this.incomeConsumptionByMonth = responseData;
+            });
+        }
+
+        get categories() {
+            return this.$store.state.categories.map(val => ({
+                text: val.name,
+                value: val.id
+            }));
         }
 
         data() {
+            const now = new Date();
+            now.setDate(now.getDate() - 365);
             return {
-                incomeConsumptionByLastYear: {},
-                incomeConsumptionPrevious: {},
+                dateFromMenu: false,
+                dateToMenu: false,
+                dateFrom: now.toISOString().substr(0, 10),
+                dateTo: new Date().toISOString().substr(0, 10),
+                incomeConsumptionByMonth: {},
+                excludeCategoryIds: [],
+                searchCategoryValue: "",
             };
         }
 
