d3fd533dad12d4b1245ea49d2f38775e3c2ff8b3
vova-uvarov <vova.uvarov@gmail.com>
2019-09-10 20:15:27 +0300

    move to template to component

--- src/components/charts/IncomAndConsumptionByMonth.vue
+++ src/components/charts/IncomAndConsumptionByMonth.vue
@@ -1,24 +1,132 @@
 <template>
-    <div>
-        <line-chart :chart-data="datacollection"
-                    :options="options"></line-chart>
-    </div>
+        <v-card>
+            <v-card-title>
+                <v-row>
+                    <v-col cols="3">
+                        <!--                todo нужно сделать компонент даты-->
+                        <v-menu
+                                v-model="dateFromMenu"
+                                :close-on-content-click="false"
+                                :nudge-right="40"
+                                transition="scale-transition"
+                                offset-y
+                                full-width
+                                min-width="290px"
+                        >
+                            <template v-slot:activator="{ on }">
+                                <v-text-field
+                                        v-model="dateFrom"
+                                        label="Дата C"
+                                        prepend-icon="event"
+                                        readonly
+                                        v-on="on"
+                                ></v-text-field>
+                            </template>
+                            <v-date-picker v-model="dateFrom"
+                                           @input="dateFromMenu = false"></v-date-picker>
+                        </v-menu>
+                    </v-col>
+                    <v-col cols="3">
+                        <!--                todo нужно сделать компонент даты-->
+                        <v-menu
+                                v-model="dateToMenu"
+                                :close-on-content-click="false"
+                                :nudge-right="40"
+                                transition="scale-transition"
+                                offset-y
+                                full-width
+                                min-width="290px"
+                        >
+                            <template v-slot:activator="{ on }">
+                                <v-text-field
+                                        v-model="dateTo"
+                                        label="Дата По"
+                                        prepend-icon="event"
+                                        readonly
+                                        v-on="on"
+                                ></v-text-field>
+                            </template>
+                            <v-date-picker v-model="dateTo"
+                                           @input="dateToMenu = false"></v-date-picker>
+                        </v-menu>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-combobox
+                                :items="categories"
+                                v-model="excludeCategoryIds"
+                                :clearable="true"
+                                multiple
+                                :search-input.sync="searchCategoryValue"
+                                @change="searchCategoryValue = ''"
+                                label="Исключить категории"
+                        ></v-combobox>
+                    </v-col>
+                    <v-col cols="3">
+                        <v-select
+                                :items="[{text: 'Месяц', value: 'MONTH'},{text: 'Год', value: 'YEAR'}]"
+                                v-model="groupBy"
+                                label="Группировка"
+                        ></v-select>
+                    </v-col>
+                </v-row>
+            </v-card-title>
+
+            <line-chart :chart-data="datacollection"
+                        :options="options"></line-chart>
+        </v-card>
 </template>
 
 <script lang="ts">
     import LineChart from "./LineChart.js";
     import {Component, Prop, Vue, Watch} from "vue-property-decorator";
+    import StatisticsService from '@/services/StatisticsService';
 
     @Component({
         components: {LineChart}
     })
     export default class IncomAndConsumptionByMonth extends Vue {
+        mounted() {
+            this.loadData();
+        }
 
-        @Prop()
-        rawData: object;
-        @Prop()
-        title: string;
+        @Watch("groupBy")
+        groupByChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
 
+        @Watch("dateFrom")
+        dateFromChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        @Watch("dateTo")
+        dateToChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        @Watch("excludeCategoryIds")
+        excludeCategoryIdsChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        private loadData() {
+            StatisticsService.incomeConsumptionByGroup(
+                this.dateFrom,
+                this.dateTo,
+                this.excludeCategoryIds,
+                this.groupBy,
+            ).then((responseData) => {
+                this.rawData = responseData;
+            });
+        }
+
+
+        get categories() {
+            return this.$store.state.categories.map(val => ({
+                text: val.name,
+                value: val.id
+            }));
+        }
         get datacollection() {
             return {
                 // labels: this.rawData.labels,
@@ -32,7 +140,7 @@
                 maintainAspectRatio: false, aspectRatio: 1,
                 title: {
                     display: true,
-                    text: this.title
+                    text: "Доход/Расход динамика"
                 },
                 legend:
                     {
@@ -43,7 +151,6 @@
                     }
             };
         }
-
 
         extractLabels(labels) {
             if (labels) {
@@ -66,6 +173,22 @@
                 });
             }
             return [];
+        }
+
+        data() {
+            const now = new Date();
+            now.setDate(now.getDate() - 365);
+            return {
+                rawData: {},
+                dateFromMenu: false,
+                dateToMenu: false,
+                dateFrom: now.toISOString().substr(0, 10),
+                dateTo: new Date().toISOString().substr(0, 10),
+                groupBy: 'MONTH',
+                incomeConsumptionByMonth: {},
+                excludeCategoryIds: [],
+                searchCategoryValue: "",
+            };
         }
 
 
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -11,80 +11,7 @@
         <v-row>
 
             <v-col cols="12">
-                <v-card>
-                    <v-card-title>
-                        <v-row>
-                            <v-col cols="3">
-                                <!--                todo нужно сделать компонент даты-->
-                                <v-menu
-                                        v-model="dateFromMenu"
-                                        :close-on-content-click="false"
-                                        :nudge-right="40"
-                                        transition="scale-transition"
-                                        offset-y
-                                        full-width
-                                        min-width="290px"
-                                >
-                                    <template v-slot:activator="{ on }">
-                                        <v-text-field
-                                                v-model="dateFrom"
-                                                label="Дата C"
-                                                prepend-icon="event"
-                                                readonly
-                                                v-on="on"
-                                        ></v-text-field>
-                                    </template>
-                                    <v-date-picker v-model="dateFrom"
-                                                   @input="dateFromMenu = false"></v-date-picker>
-                                </v-menu>
-                            </v-col>
-                            <v-col cols="3">
-                                <!--                todo нужно сделать компонент даты-->
-                                <v-menu
-                                        v-model="dateToMenu"
-                                        :close-on-content-click="false"
-                                        :nudge-right="40"
-                                        transition="scale-transition"
-                                        offset-y
-                                        full-width
-                                        min-width="290px"
-                                >
-                                    <template v-slot:activator="{ on }">
-                                        <v-text-field
-                                                v-model="dateTo"
-                                                label="Дата По"
-                                                prepend-icon="event"
-                                                readonly
-                                                v-on="on"
-                                        ></v-text-field>
-                                    </template>
-                                    <v-date-picker v-model="dateTo"
-                                                   @input="dateToMenu = false"></v-date-picker>
-                                </v-menu>
-                            </v-col>
-                            <v-col cols="3">
-                                <v-combobox
-                                        :items="categories"
-                                        v-model="excludeCategoryIds"
-                                        :clearable="true"
-                                        multiple
-                                        :search-input.sync="searchCategoryValue"
-                                        @change="searchCategoryValue = ''"
-                                        label="Исключить категории"
-                                ></v-combobox>
-                            </v-col>
-                            <v-col cols="3">
-                                <v-select
-                                        :items="[{text: 'Месяц', value: 'MONTH'},{text: 'Год', value: 'YEAR'}]"
-                                        v-model="groupBy"
-                                        label="Группировка"
-                                ></v-select>
-                            </v-col>
-                        </v-row>
-                    </v-card-title>
-                    <IncomAndConsumptionByMonth :raw-data="incomeConsumptionByMonth"
-                                                title="Доход/Расход по месяцам"/>
-                </v-card>
+                <IncomAndConsumptionByMonth/>
             </v-col>
         </v-row>
 
@@ -94,8 +21,7 @@
 <script lang="ts">
     import IncomAndConsumptionByMonth from "../components/charts/IncomAndConsumptionByMonth.vue";
     import AccountBalancesPie from "../components/charts/AccountBalancesPie.vue";
-    import {Component, Vue, Watch} from "vue-property-decorator";
-    import StatisticsService from '@/services/StatisticsService';
+    import {Component, Vue} from "vue-property-decorator";
 
     @Component({
         components: {
@@ -103,62 +29,7 @@
         }
     })
     export default class StatisticsVue extends Vue {
-        mounted() {
-            this.loadData();
-        }
 
-        @Watch("groupBy")
-        groupByChanged(value: string, oldValue: string) {
-            this.loadData();
-        }
-
-        @Watch("dateFrom")
-        dateFromChanged(value: string, oldValue: string) {
-            this.loadData();
-        }
-
-        @Watch("dateTo")
-        dateToChanged(value: string, oldValue: string) {
-            this.loadData();
-        }
-
-        @Watch("excludeCategoryIds")
-        excludeCategoryIdsChanged(value: string, oldValue: string) {
-            this.loadData();
-        }
-
-        private loadData() {
-            StatisticsService.incomeConsumptionByGroup(
-                this.dateFrom,
-                this.dateTo,
-                this.excludeCategoryIds,
-                this.groupBy,
-            ).then((responseData) => {
-                this.incomeConsumptionByMonth = responseData;
-            });
-        }
-
-        get categories() {
-            return this.$store.state.categories.map(val => ({
-                text: val.name,
-                value: val.id
-            }));
-        }
-
-        data() {
-            const now = new Date();
-            now.setDate(now.getDate() - 365);
-            return {
-                dateFromMenu: false,
-                dateToMenu: false,
-                dateFrom: now.toISOString().substr(0, 10),
-                dateTo: new Date().toISOString().substr(0, 10),
-                groupBy: 'MONTH',
-                incomeConsumptionByMonth: {},
-                excludeCategoryIds: [],
-                searchCategoryValue: "",
-            };
-        }
 
     }
 </script>
