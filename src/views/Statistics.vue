50eaff7f0c6b6dc4a43ae55dc9a839409bbf12da
vova.uvarov <vova.uvarov@bssys.com>
2019-09-10 19:59:19 +0300

    ~

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -9,76 +9,79 @@
 
         </v-row>
         <v-row>
-            <v-row>
-                <v-col cols="3">
-                    <!--                todo нужно сделать компонент даты-->
-                    <v-menu
-                            v-model="dateFromMenu"
-                            :close-on-content-click="false"
-                            :nudge-right="40"
-                            transition="scale-transition"
-                            offset-y
-                            full-width
-                            min-width="290px"
-                    >
-                        <template v-slot:activator="{ on }">
-                            <v-text-field
-                                    v-model="dateFrom"
-                                    label="Дата C"
-                                    prepend-icon="event"
-                                    readonly
-                                    v-on="on"
-                            ></v-text-field>
-                        </template>
-                        <v-date-picker v-model="dateFrom"
-                                       @input="dateFromMenu = false"></v-date-picker>
-                    </v-menu>
-                </v-col>
-                <v-col cols="3">
-                    <!--                todo нужно сделать компонент даты-->
-                    <v-menu
-                            v-model="dateToMenu"
-                            :close-on-content-click="false"
-                            :nudge-right="40"
-                            transition="scale-transition"
-                            offset-y
-                            full-width
-                            min-width="290px"
-                    >
-                        <template v-slot:activator="{ on }">
-                            <v-text-field
-                                    v-model="dateTo"
-                                    label="Дата По"
-                                    prepend-icon="event"
-                                    readonly
-                                    v-on="on"
-                            ></v-text-field>
-                        </template>
-                        <v-date-picker v-model="dateTo"
-                                       @input="dateToMenu = false"></v-date-picker>
-                    </v-menu>
-                </v-col>
-                <v-col cols="3">
-                    <v-combobox
-                            :items="categories"
-                            v-model="excludeCategoryIds"
-                            :clearable="true"
-                            multiple
-                            :search-input.sync="searchCategoryValue"
-                            @change="searchCategoryValue = ''"
-                            label="Исключить категории"
-                    ></v-combobox>
-                </v-col>
-                <v-col cols="3">
-                    <v-select
-                            :items="[{text: 'Месяц', value: 'MONTH'},{text: 'Год', value: 'YEAR'}]"
-                            v-model="groupBy"
-                            label="Группировка"
-                    ></v-select>
-                </v-col>
-            </v-row>
+
             <v-col cols="12">
                 <v-card>
+                    <v-card-title>
+                        <v-row>
+                            <v-col cols="3">
+                                <!--                todo нужно сделать компонент даты-->
+                                <v-menu
+                                        v-model="dateFromMenu"
+                                        :close-on-content-click="false"
+                                        :nudge-right="40"
+                                        transition="scale-transition"
+                                        offset-y
+                                        full-width
+                                        min-width="290px"
+                                >
+                                    <template v-slot:activator="{ on }">
+                                        <v-text-field
+                                                v-model="dateFrom"
+                                                label="Дата C"
+                                                prepend-icon="event"
+                                                readonly
+                                                v-on="on"
+                                        ></v-text-field>
+                                    </template>
+                                    <v-date-picker v-model="dateFrom"
+                                                   @input="dateFromMenu = false"></v-date-picker>
+                                </v-menu>
+                            </v-col>
+                            <v-col cols="3">
+                                <!--                todo нужно сделать компонент даты-->
+                                <v-menu
+                                        v-model="dateToMenu"
+                                        :close-on-content-click="false"
+                                        :nudge-right="40"
+                                        transition="scale-transition"
+                                        offset-y
+                                        full-width
+                                        min-width="290px"
+                                >
+                                    <template v-slot:activator="{ on }">
+                                        <v-text-field
+                                                v-model="dateTo"
+                                                label="Дата По"
+                                                prepend-icon="event"
+                                                readonly
+                                                v-on="on"
+                                        ></v-text-field>
+                                    </template>
+                                    <v-date-picker v-model="dateTo"
+                                                   @input="dateToMenu = false"></v-date-picker>
+                                </v-menu>
+                            </v-col>
+                            <v-col cols="3">
+                                <v-combobox
+                                        :items="categories"
+                                        v-model="excludeCategoryIds"
+                                        :clearable="true"
+                                        multiple
+                                        :search-input.sync="searchCategoryValue"
+                                        @change="searchCategoryValue = ''"
+                                        label="Исключить категории"
+                                ></v-combobox>
+                            </v-col>
+                            <v-col cols="3">
+                                <v-select
+                                        :items="[{text: 'Месяц', value: 'MONTH'},{text: 'Год', value: 'YEAR'}]"
+                                        v-model="groupBy"
+                                        label="Группировка"
+                                ></v-select>
+                            </v-col>
+                        </v-row>
+                    </v-card-title>
                     <IncomAndConsumptionByMonth :raw-data="incomeConsumptionByMonth"
                                                 title="Доход/Расход по месяцам"/>
                 </v-card>
