102d44a980ba908cc964a68eb813907c203cfae1
vova-uvarov <vova.uvarov@gmail.com>
2019-09-10 20:05:21 +0300

    js->ts

--- src/components/charts/IncomAndConsumptionByMonth.vue
+++ src/components/charts/IncomAndConsumptionByMonth.vue
@@ -5,82 +5,84 @@
     </div>
 </template>
 
-<script>
-    import LineChart from './LineChart.js'
+<script lang="ts">
+    import LineChart from "./LineChart.js";
+    import {Component, Prop, Vue, Watch} from "vue-property-decorator";
 
-    export default {
-        components: {
-            LineChart
-        },
-        props: ['rawData','title'],
+    @Component({
+        components: {LineChart}
+    })
+    export default class IncomAndConsumptionByMonth extends Vue {
 
-        computed: {
-            datacollection: function () {
-                return {
-                    // labels: this.rawData.labels,
-                    labels: this.extractLabels(this.rawData.labels),
-                    datasets: this.extractDatasets(this.rawData.datasets)
-                }
-            },
-            options: function () {
+        @Prop()
+        rawData: object;
+        @Prop()
+        title: string;
 
-                return {
-                    maintainAspectRatio: false, aspectRatio: 1,
-                    title: {
-                        display: true,
-                        text: this.title
-                    },
-                    legend:
-                        {
-                            position: 'bottom',
-                            labels: {
-                                usePointStyle: true
-                            }
-                        }
-                }
-            }
-        },
-
-        data() {
-            return {}
-        },
-
-        methods: {
-            extractLabels(labels) {
-                if (labels) {
-                    return labels;
-                }
-                return [];
-            },
-            extractDatasets(datasets) {
-                if (datasets) {
-                    return datasets.map((item) => {
-                        return {
-                            lineTension: 0,
-                            label: item.name,
-                            fill: false,
-                            borderColor: this.getColorByDataSetName(item.name),
-                            data: item.data
-                        }
-                    });
-                }
-                return [];
-            },
-            getColorByDataSetName(dataSetName) {
-                if (dataSetName === 'Доход') {
-                    return "green";
-                }
-                if (dataSetName === 'Расход') {
-                    return "red";
-                }
-                return "black"
-            }
+        get datacollection() {
+            return {
+                // labels: this.rawData.labels,
+                labels: this.extractLabels(this.rawData.labels),
+                datasets: this.extractDatasets(this.rawData.datasets)
+            };
         }
+
+        get options() {
+            return {
+                maintainAspectRatio: false, aspectRatio: 1,
+                title: {
+                    display: true,
+                    text: this.title
+                },
+                legend:
+                    {
+                        position: "bottom",
+                        labels: {
+                            usePointStyle: true
+                        }
+                    }
+            };
+        }
+
+
+        extractLabels(labels) {
+            if (labels) {
+                return labels;
+            }
+            return [];
+        }
+
+
+        extractDatasets(datasets) {
+            if (datasets) {
+                return datasets.map((item) => {
+                    return {
+                        lineTension: 0,
+                        label: item.name,
+                        fill: false,
+                        borderColor: this.getColorByDataSetName(item.name),
+                        data: item.data
+                    };
+                });
+            }
+            return [];
+        }
+
+
+        getColorByDataSetName(dataSetName) {
+            if (dataSetName === "Доход") {
+                return "green";
+            }
+            if (dataSetName === "Расход") {
+                return "red";
+            }
+            return "black";
+        }
+
 
     }
 </script>
 
 <style>
-    .small {
-    }
+
 </style>
