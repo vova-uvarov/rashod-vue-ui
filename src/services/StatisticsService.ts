adba29435c4e8b3d4898393f7938f0a4aa8d0819
uvarovvy <uvarov.vy@reliab.tech>
2019-09-10 11:24:00 +0300

    Доход/Расход по месяцам с выбором даты и исключающих категорий

--- src/services/StatisticsService.ts
+++ src/services/StatisticsService.ts
@@ -2,18 +2,20 @@ import axios, {AxiosResponse} from 'axios';
 
 export default class StatisticsService {
 
-    public static incomeConsumptionByMonthLastYear(): Promise<AxiosResponse<object>> {
+    public static incomeConsumptionByMonth(dateFrom, dateTo, excludeCategoryIds): Promise<AxiosResponse<object>> {
+        if (excludeCategoryIds) { // todo это из за того что axios массивы передает как nam[]=1,2,3 при этом spring так не ждем
+            excludeCategoryIds = excludeCategoryIds.map((item) => (item.value)).join(',');
+        }
         return axios
-            .get('http://localhost:8092/api/statistics/incomeConsumptionByMonthLastYear')
+            .get('http://localhost:8092/api/statistics/incomeConsumptionByMonth', {
+                params: {
+                    from: dateFrom,
+                    to: dateTo,
+                    excludeCategoryIds: excludeCategoryIds
+                }
+            })
             .then((response) => (response.data));
     }
-
-    public static incomeConsumptionByMonthLastPrevious(): Promise<AxiosResponse<object>> {
-        return axios
-            .get('http://localhost:8092/api/statistics/incomeConsumptionByMonthPrevious')
-            .then((response) => (response.data));
-    }
-
 
 
 }
