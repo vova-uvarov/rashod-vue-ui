69fcac224a0161044c4fdb4b09e7449547d8b5a1
vova-uvarov <vova.uvarov@gmail.com>
2019-09-11 08:49:29 +0300

    average month trend

--- src/services/StatisticsService.ts
+++ src/services/StatisticsService.ts
@@ -2,6 +2,19 @@ import axios, {AxiosResponse} from 'axios';
 
 export default class StatisticsService {
 
+    public static averageByDayInCurrMonth(excludeCategoryIds): Promise<AxiosResponse<object>> {
+        if (excludeCategoryIds) { // todo это из за того что axios массивы передает как nam[]=1,2,3 при этом spring так не ждем
+            excludeCategoryIds = excludeCategoryIds.map((item) => (item.value)).join(',');
+        }
+        return axios
+            .get('http://localhost:8092/api/statistics/averageByDayInCurrMonth', {
+                params: {
+                    excludeCategoryIds: excludeCategoryIds,
+                },
+            })
+            .then((response) => (response.data));
+    }
+
     public static monthPlan(): Promise<AxiosResponse<object>> {
         return axios
             .get('http://localhost:8092/api/statistics/plan/month')
