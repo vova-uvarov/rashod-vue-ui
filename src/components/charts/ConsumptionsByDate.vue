5801b386bc25b2c910705ef2bb09f9033c3b0891
vova-uvarov <vova.uvarov@gmail.com>
2019-09-11 08:13:06 +0300

    consumption by date chart

---
+++ src/components/charts/ConsumptionsByDate.vue
@@ -0,0 +1,234 @@
+<template>
+    <v-card>
+        <v-chip class="ma-2" color="primary" outlined>
+            <b>План на месяц:</b> {{monthPlan.totalPlan|moneyFormat}}
+        </v-chip>
+        <v-chip class="ma-2" color="primary" outlined>
+            <b>План на день:</b> {{monthPlan.planForDay|moneyFormat}}
+        </v-chip>
+
+        <v-chip class="ma-2" color="primary" outlined>
+            <b>В среднем тратим:</b> {{monthPlan.currentAverageSum|moneyFormat}}
+        </v-chip>
+
+        <v-chip class="ma-2" color="primary" outlined>
+            <b>Можно потратить:</b> {{monthPlan.canSpent|moneyFormat}}
+        </v-chip>
+
+        <v-chip class="ma-2" color="primary" outlined>
+            <b>Выйти в ноль:</b> {{monthPlan.canSpendForPlan|moneyFormat}}
+        </v-chip>
+
+        <v-card-title>
+            <v-row>
+                <v-col cols="4">
+                    <!--                todo нужно сделать компонент даты-->
+                    <v-menu
+                            v-model="dateFromMenu"
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            transition="scale-transition"
+                            offset-y
+                            full-width
+                            min-width="290px"
+                    >
+                        <template v-slot:activator="{ on }">
+                            <v-text-field
+                                    v-model="dateFrom"
+                                    label="Дата C"
+                                    prepend-icon="event"
+                                    readonly
+                                    v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="dateFrom"
+                                       @input="dateFromMenu = false"></v-date-picker>
+                    </v-menu>
+                </v-col>
+                <v-col cols="4">
+                    <!--                todo нужно сделать компонент даты-->
+                    <v-menu
+                            v-model="dateToMenu"
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            transition="scale-transition"
+                            offset-y
+                            full-width
+                            min-width="290px"
+                    >
+                        <template v-slot:activator="{ on }">
+                            <v-text-field
+                                    v-model="dateTo"
+                                    label="Дата По"
+                                    prepend-icon="event"
+                                    readonly
+                                    v-on="on"
+                            ></v-text-field>
+                        </template>
+                        <v-date-picker v-model="dateTo"
+                                       @input="dateToMenu = false"></v-date-picker>
+                    </v-menu>
+                </v-col>
+                <v-col cols="4">
+                    <v-combobox
+                            :items="categories"
+                            v-model="excludeCategoryIds"
+                            :clearable="true"
+                            multiple
+                            :search-input.sync="searchCategoryValue"
+                            @change="searchCategoryValue = ''"
+                            label="Исключить категории"
+                    ></v-combobox>
+                </v-col>
+            </v-row>
+        </v-card-title>
+
+        <bar-chart :chart-data="datacollection"
+                   :options="options"></bar-chart>
+    </v-card>
+</template>
+
+<script lang="ts">
+    import BarChart from "./BarChart.js";
+    import {Component, Prop, Vue, Watch} from "vue-property-decorator";
+    import StatisticsService from "@/services/StatisticsService";
+
+    @Component({
+        components: {BarChart}
+    })
+    export default class IncomAndConsumptionTrend extends Vue {
+        mounted() {
+            this.loadData();
+            StatisticsService.monthPlan()
+                .then((responseData) => {
+                    this.monthPlan = responseData;
+                });
+        }
+
+        @Watch("dateFrom")
+        dateFromChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        @Watch("dateTo")
+        dateToChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        @Watch("excludeCategoryIds")
+        excludeCategoryIdsChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        private loadData() {
+            StatisticsService.incomeConsumptionByGroup(
+                this.dateFrom,
+                this.dateTo,
+                this.excludeCategoryIds,
+                this.groupBy,
+            ).then((responseData) => {
+                this.rawData = responseData;
+            });
+        }
+
+
+        get categories() {
+            return this.$store.state.categories.map(val => ({
+                text: val.name,
+                value: val.id
+            }));
+        }
+
+        get datacollection() {
+            return {
+                // labels: this.rawData.labels,
+                labels: this.extractLabels(this.rawData.labels),
+                datasets: this.extractDatasets(this.rawData.datasets)
+            };
+        }
+
+        get options() {
+            return {
+                maintainAspectRatio: false, aspectRatio: 1,
+                title: {
+                    display: true,
+                    text: "Расход по дня"
+                },
+                legend:
+                    {
+                        position: "bottom",
+                        labels: {
+                            usePointStyle: true
+                        }
+                    }
+            };
+        }
+
+        extractLabels(labels) {
+            if (labels) {
+                return labels;
+            }
+            return [];
+        }
+
+
+        extractDatasets(datasets) {
+            if (datasets) {
+                return datasets.filter((item) => (item.name == "Расход"))
+                    .map((item) => {
+                        return {
+                            lineTension: 0,
+                            label: item.name,
+                            fill: false,
+                            borderColor: this.getColorByDataSetName(item.name),
+                            backgroundColor: item.data.map((d) => {
+                                if (d < 1000) {
+                                    return "green";
+                                }
+                                if (d < 2000) {
+                                    return "orange";
+                                }
+                                return "red";
+                            }),
+                            data: item.data
+                        };
+                    });
+            }
+            return [];
+        }
+
+        data() {
+            const now = new Date();
+            now.setDate(now.getDate() - 31);
+            return {
+                monthPlan: {},
+                rawData: {},
+                dateFromMenu: false,
+                dateToMenu: false,
+                dateFrom: now.toISOString().substr(0, 10),
+                dateTo: new Date().toISOString().substr(0, 10),
+                groupBy: "DAY",
+                incomeConsumptionByMonth: {},
+                excludeCategoryIds: [{"text": "ИП", "value": 15}], // todo
+                searchCategoryValue: "",
+            };
+        }
+
+
+        getColorByDataSetName(dataSetName) {
+            if (dataSetName === "Доход") {
+                return "green";
+            }
+            if (dataSetName === "Расход") {
+                return "red";
+            }
+            return "black";
+        }
+
+
+    }
+</script>
+
+<style>
+
+</style>
--- src/services/StatisticsService.ts
+++ src/services/StatisticsService.ts
@@ -2,6 +2,12 @@ import axios, {AxiosResponse} from 'axios';
 
 export default class StatisticsService {
 
+    public static monthPlan(): Promise<AxiosResponse<object>> {
+        return axios
+            .get('http://localhost:8092/api/statistics/plan/month')
+            .then((response) => (response.data));
+    }
+
     public static incomeConsumptionByGroup(dateFrom, dateTo, excludeCategoryIds, groupBy): Promise<AxiosResponse<object>> {
         if (excludeCategoryIds) { // todo это из за того что axios массивы передает как nam[]=1,2,3 при этом spring так не ждем
             excludeCategoryIds = excludeCategoryIds.map((item) => (item.value)).join(',');
