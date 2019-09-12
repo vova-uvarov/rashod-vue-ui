41d5204db544c917a213e694e7911225c43d2f9f
vova.uvarov <vova.uvarov@bssys.com>
2019-09-12 08:58:13 +0300

    categoryTrend

--- src/router.ts
+++ src/router.ts
@@ -36,6 +36,14 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/Statistics.vue'),
     },
     {
+      path: '/statistics/categoryTrend',
+      name: 'statisticsCategoryTrend',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/statistics/CategoryTrend.vue'),
+    },
+    {
       path: '/plans',
       name: 'plans',
       // route level code-splitting
