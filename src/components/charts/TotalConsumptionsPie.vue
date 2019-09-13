8ac977aadf610c7cc06eb8c2eef8e38b199ea32d
vova-uvarov <vova.uvarov@gmail.com>
2019-09-13 08:05:16 +0300

    Набросок статистики по годам.

--- src/components/charts/TotalConsumptionsPie.vue
+++ src/components/charts/TotalConsumptionsPie.vue
@@ -1,69 +1,122 @@
 <template>
     <div class="small">
+        dateFrom={{dateFrom}} <br/>
+        dateTo={{dateTo}}<br/>
+        consumptionsByCategory={{consumptionsByCategory}}<br/>
+        datacollection={{datacollection}}
         <PieChart :chart-data="datacollection" :options="options"></PieChart>
     </div>
 </template>
 
-<script>
-    import PieChart from './PieChart.js'
+<script lang="ts">
+    import PieChart from "./PieChart.js";
     import StatisticsService from "@/services/StatisticsService";
+    import {Component, Prop, Vue, Watch} from "vue-property-decorator";
 
-    export default {
-        components: {
-            PieChart
-        },
+    @Component({
+        components: {PieChart}
+    })
+    export default class TotalConsumptionPie extends Vue {
+        @Prop()
+        year: number;
+
+        @Prop()
+        dateFrom: any;
+
+        @Prop()
+        dateTo: any;
+
+        @Prop()
+        operationType: string;
+
+        @Prop()
+        title: string;
+
+
         data() {
             return {
                 balances: [],
-                datacollection: null,
+                datacollection: {},
                 options: {
                     title: {
                         display: true,
-                        text: "Расходы за все время"
+                        text: this.title
                     },
                     legend:
                         {
-                            position: 'bottom',
+                            position: "bottom",
                             labels: {
                                 usePointStyle: true
                             }
                         }
                 }
-            }
-        },
-        mounted() {
+            };
+        }
 
-            StatisticsService.sumsGroupByCategory({operationTypes: ["CONSUMPTION"]})
+        @Watch("year")
+        yearChanged(value: string, oldValue: string) {
+            this.loadData();
+        }
+
+        mounted() {
+            this.loadData();
+        }
+
+        private loadData() {
+            let dateFrom = null;
+            let dateTo = null;
+            if (this.year) {
+                dateFrom = this.year + "-01-01"; // todo стремная штука
+                dateTo = this.year + "-12-31";// todo стремная штука
+            } else {
+                if (this.dateFrom) {
+                    dateFrom = this.dateFrom;
+                }
+                if (this.dateTo) {
+                    dateTo = this.dateTo;
+                }
+            }
+            StatisticsService.sumsGroupByCategory(
+                {
+                    operationTypes: [this.operationType],
+                    from: dateFrom,
+                    to: dateTo
+                })
                 .then((data) => {
                     this.consumptionsByCategory = data;
-                    this.fillData()
+                    this.fillData();
                 });
-        },
-        methods: {
-            fillData() {
-                this.datacollection = {
-                    datasets: [{
-                        backgroundColor: this.consumptionsByCategory.map((a) => (this.getRandomColor())),
-                        data: this.consumptionsByCategory.map((a) => (a.data[0])) // 0 для pie всегда один элемент
-                    }],
-
-
-                    // These labels appear in the legend and in the tooltips when hovering different arcs
-                    labels: this.consumptionsByCategory.map((a) => (a.name))
-
-
-                }
-            },
-            getRandomColor() {
-                var letters = '0123456789ABCDEF';
-                var color = '#';
-                for (var i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
-                }
-                return color;
-            },
         }
-    }
+
+        fillData() {
+        // |     property 'datasets' -> object with constructor 'Array'
+        //     |     index 0 -> object with constructor 'Object'
+        //     |     ...
+        // |     property 'config' -> object with constructor 'Object'
+        //     --- property 'data' closes the circle"
+            this.datacollection = {
+                datasets: [{
+                    backgroundColor: this.consumptionsByCategory.map((a) => (this.getRandomColor())),
+                    data: this.consumptionsByCategory.map((a) => (a.data[0])) // 0 для pie всегда один элемент
+                }],
+
+
+                // These labels appear in the legend and in the tooltips when hovering different arcs
+                labels: this.consumptionsByCategory.map((a) => (a.name))
+
+
+            };
+        }
+
+        getRandomColor() {
+            var letters = "0123456789ABCDEF";
+            var color = "#";
+            for (var i = 0; i < 6; i++) {
+                color += letters[Math.floor(Math.random() * 16)];
+            }
+            return color;
+        }
+    };
 </script>
 
 <style>
---
+++ src/views/statistics/ByYear.vue
@@ -0,0 +1,68 @@
+<template>
+    <v-container fluid class="pa-2">
+        months = {{months}}
+        <v-row>
+            <v-col cols="12">
+                <v-select
+                        :items="years"
+                        v-model="year"
+                        :clearable="false"
+                        label="Год"
+                ></v-select>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col cols="6">
+                <TotalConsumptionsPie operation-type="CONSUMPTION" title="Расходы за год" :year="year"/>
+            </v-col>
+            <v-col cols="6">
+                <TotalConsumptionsPie operation-type="INCOME" title="Доходы за год" :year="year"/>
+            </v-col>
+        </v-row>
+
+        <v-row v-for="rowIndex in [0,1,2,3]">
+            <v-col cols="4" v-for="colIndex in [0,1,2]" :key="colIndex">
+                <TotalConsumptionsPie operation-type="CONSUMPTION" :title="'Расходы за: '+monthNames[rowIndex*3+colIndex] "
+                                      :date-from="months[rowIndex*3+colIndex].from" :date-to="months[rowIndex*3+colIndex].to"/>
+            </v-col>
+        </v-row>
+    </v-container>
+
+</template>
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import TotalConsumptionsPie from "@/components/charts/TotalConsumptionsPie.vue";
+
+    @Component({
+        components: {
+            TotalConsumptionsPie
+        }
+    })
+    export default class StatisticsVue extends Vue {
+
+        get monthNames() {
+            return ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"]
+        }
+        get months() {
+            let months = [];
+            for (let i = 0; i < 12; i++) {
+                months.push({
+                    from: new Date(this.year, i, 1, 3, 0, 0).toISOString().substr(0, 10), // todo тут надо с часовыми поясами разобраться
+                    to: new Date(this.year, i + 1, 0, 23, 59, 59).toISOString().substr(0, 10), // todo тут надо с часовыми поясами разобраться
+                });
+            }
+            return months;
+        }
+
+        get years() {
+            return [2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012]; // todo Стоит этот список генерировать
+        }
+
+        data() {
+            return {
+                year: 2019 // todo нужн брать текущий год
+            };
+        }
+    }
+</script>
+
