c39a92ace5b6b8ce8272022a206ad92eef3aa4c3
vova-uvarov <vova.uvarov@gmail.com>
2019-09-16 22:57:46 +0300

    create select date component

--- src/components/charts/IncomAndConsumptionTrend.vue
+++ src/components/charts/IncomAndConsumptionTrend.vue
@@ -3,52 +3,10 @@
         <v-card-title>
             <v-row>
                 <v-col cols="3">
-                    <!--                todo нужно сделать компонент даты-->
-                    <v-menu
-                            v-model="dateFromMenu"
-                            :close-on-content-click="false"
-                            :nudge-right="40"
-                            transition="scale-transition"
-                            offset-y
-                            full-width
-                            min-width="290px"
-                    >
-                        <template v-slot:activator="{ on }">
-                            <v-text-field
-                                    v-model="dateFrom"
-                                    label="Дата C"
-                                    prepend-icon="event"
-                                    readonly
-                                    v-on="on"
-                            ></v-text-field>
-                        </template>
-                        <v-date-picker v-model="dateFrom"
-                                       @input="dateFromMenu = false"></v-date-picker>
-                    </v-menu>
+                    <select-date label="Дата C" :date.sync="dateFrom"/>
                 </v-col>
                 <v-col cols="3">
-                    <!--                todo нужно сделать компонент даты-->
-                    <v-menu
-                            v-model="dateToMenu"
-                            :close-on-content-click="false"
-                            :nudge-right="40"
-                            transition="scale-transition"
-                            offset-y
-                            full-width
-                            min-width="290px"
-                    >
-                        <template v-slot:activator="{ on }">
-                            <v-text-field
-                                    v-model="dateTo"
-                                    label="Дата По"
-                                    prepend-icon="event"
-                                    readonly
-                                    v-on="on"
-                            ></v-text-field>
-                        </template>
-                        <v-date-picker v-model="dateTo"
-                                       @input="dateToMenu = false"></v-date-picker>
-                    </v-menu>
+                    <select-date label="Дата По" :date.sync="dateTo"/>
                 </v-col>
                 <v-col cols="3">
                     <v-combobox
@@ -81,10 +39,9 @@
 import LineChart from './js/LineChart.js';
 import {Component, Vue, Watch} from 'vue-property-decorator';
 import StatisticsService from '@/services/StatisticsService';
+import SelectDate from "@/components/common/SelectDate.vue";
 
-@Component({
-    components: {LineChart},
-})
+@Component({    components: {LineChart,SelectDate},})
 export default class IncomAndConsumptionTrend extends Vue {
 
 
