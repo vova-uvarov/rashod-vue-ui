9e7751570c88ba83571cb3710694d77eb4879f3c
vova.uvarov <vova.uvarov@gmail.com>
2020-03-22 11:17:09 +0300

    build with use Dockerfile

--- src/services/LoginService.ts
+++ src/services/LoginService.ts
@@ -5,13 +5,13 @@ export default class LoginService {
     public static login(userName: string, password: string): Promise<AxiosResponse<any>> {
         // todo
         const headers = {
-            'Authorization': 'Basic dWk6dWlfc2VjcmV0'
-        }
+            Authorization: process.env.VUE_APP_AUTH_CLIENT_HEADER,
+        };
 
         return axios
             // todo
-            .post('http://localhost:9999/oauth/token?username=' + userName + '&password=' + password + '&grant_type=password',{}, {
-                headers: headers
+            .post(process.env.VUE_APP_AUTH_API_URL + '?username=' + userName + '&password=' + password + '&grant_type=password', {}, {
+                headers,
             })
             .then((response) => (response.data));
     }
