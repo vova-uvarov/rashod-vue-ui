ea97b2577810954065ab8d2f6420ab22d15ef32d
vuvarov <vova.uvarov@gmail.com>
2020-05-31 17:23:46 +0300

    fix #125 button for add operation into operation list

---
+++ src/components/operation/CreateOperationDialog.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-row justify="center">
+        <v-dialog
+                max-width="790"
+                v-model="showDialog"
+        >
+            <CreateOperationComponent/>
+        </v-dialog>
+    </v-row>
+</template>
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import CreateOperationComponent from '@/components/operation/CreateOperationComponent';
+
+    @Component({
+        components: {CreateOperationComponent}
+    })
+    export default class CreateOperationDialog extends Vue {
+
+        get showDialog() {
+            return this.visible;
+        }
+
+        @Prop()
+        public visible!: string;
+    }
+</script>
--- src/components/operation/OperationsList.vue
+++ src/components/operation/OperationsList.vue
@@ -1,18 +1,18 @@
 <template>
     <v-row justify="center" align="start">
-       <v-col cols="6" offset="3"  v-if="loading">
-           <v-progress-circular
+        <v-col cols="6" offset="3" v-if="loading">
+            <v-progress-circular
 
-                   :indeterminate="true"
-                   :rotate="0"
-                   :size="32"
-                   :value="0"
-                   :width="4"
-                   color="light-blue"
-           />
-           Идет загрузка данных...
-       </v-col>
-        <v-col cols="12" >
+                    :indeterminate="true"
+                    :rotate="0"
+                    :size="32"
+                    :value="0"
+                    :width="4"
+                    color="light-blue"
+            />
+            Идет загрузка данных...
+        </v-col>
+        <v-col cols="12">
             <v-simple-table :dense="true">
                 <thead>
                 <tr>
@@ -27,13 +27,17 @@
                     <th class="text-left">Комментарий</th>
                     <th class="text-left">Автор</th>
                     <th class="text-left">План</th>
-                    <th class="text-left"></th>
+                    <th class="text-left">
+                        <v-btn class="mx-2" fab dark x-small color="primary" @click="createOperation()">
+                            <v-icon dark>mdi-plus</v-icon>
+                        </v-btn>
+                    </th>
                 </tr>
                 </thead>
                 <tbody>
                 <tr v-for="(item, index) in operations" :key="item.name" :class="rowStyle(item,index)">
                     <td>
-                        <v-btn class="mx-2" fab dark small color="primary" @click="deleteOperation(item.id)">
+                        <v-btn class="mx-2" fab dark x-small color="primary" @click="deleteOperation(item.id)">
                             <v-icon dark>mdi-minus</v-icon>
                         </v-btn>
                     </td>
@@ -56,12 +60,12 @@
                     </td>
 
                     <td style="white-space: nowrap; width: 1%">
-                        <v-btn class="mx-2" fab dark small color="primary" @click.stop="editOperation(item.id)">
-                            <v-icon>mdi-file-document-edit</v-icon>
+                        <v-btn x-small class="mx-2" fab dark color="primary">
+                            <v-icon @click="editOperation(item.id)">mdi-file-document-edit</v-icon>
                         </v-btn>
 
 
-                        <v-btn class="mx-2" fab dark small color="primary" @click.stop="divideOperation(item.id)">
+                        <v-btn class="mx-2" fab dark x-small color="primary" @click.stop="divideOperation(item.id)">
                             <v-icon>mdi-call-split</v-icon>
                         </v-btn>
                     </td>
@@ -70,6 +74,7 @@
                 </tbody>
             </v-simple-table>
         </v-col>
+        <CreateOperationDialog :visible="showCreateDialog" @close="showCreateDialog=false"/>
         <EditOperationDialog :operationId="operationId" :visible="showEditDialog" @close="showEditDialog=false"/>
         <DivideOperationDialog :parentOperationId="operationId"
                                :visible="showDivideDialog"
@@ -77,79 +82,85 @@
     </v-row>
 </template>
 <script lang="ts">
-import {Component, Prop, Vue} from 'vue-property-decorator';
-import EditOperationDialog from '@/components/operation/EditOperationDialog.vue';
-import OperationService from '@/services/OperationService';
-import OperationUtils from '@/utils/OperationUtils';
-import DivideOperationDialog from '@/components/operation/DivideOperationDialog.vue';
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+    import CreateOperationDialog from '@/components/operation/CreateOperationDialog.vue';
+    import EditOperationDialog from '@/components/operation/EditOperationDialog.vue';
+    import OperationService from '@/services/OperationService';
+    import OperationUtils from '@/utils/OperationUtils';
+    import DivideOperationDialog from '@/components/operation/DivideOperationDialog.vue';
 
-@Component({
-    components: {EditOperationDialog, DivideOperationDialog},
-    methods: {
-        getCostSign(item) {
-            return OperationUtils.getCostSign(item);
-        },
-        rowStyle(item, index) {
-            let mainClass = '';
-            if (item.operationType === 'CONSUMPTION') {
-                mainClass = 'consumptionRow';
+    @Component({
+        components: {EditOperationDialog, DivideOperationDialog, CreateOperationDialog},
+        methods: {
+            getCostSign(item: any) {
+                return OperationUtils.getCostSign(item);
+            },
+            rowStyle(item: any, index: any) {
+                let mainClass = '';
+                if (item.operationType === 'CONSUMPTION') {
+                    mainClass = 'consumptionRow';
+                }
+
+                if (item.operationType === 'TRANSFER') {
+                    mainClass = 'transfer';
+                }
+
+                if (item.operationType === 'INCOME') {
+                    mainClass = 'income';
+                }
+
+                if (index % 2 === 0) {
+                    return mainClass + ' odd';
+                } else {
+                    return mainClass + ' even';
+                }
             }
+        }
+    })
+    export default class OperationsList extends Vue {
 
-            if (item.operationType === 'TRANSFER') {
-                mainClass = 'transfer';
+        @Prop({default: []})
+        public operations!: object[];
+
+        @Prop({default: false})
+        public loading!: boolean;
+
+        public operationId = null;
+        public showEditDialog = false;
+        public showDivideDialog = false;
+        public showCreateDialog = false;
+
+        public deleteOperation(id: string) {
+            if (confirm('Вы действительно хотите удалить операцию?')) {
+                OperationService.delete(id)
+                    .then((response) => {
+                            this.$root.$emit('operationChanged');
+                        }
+                    );
             }
+        }
 
-            if (item.operationType === 'INCOME') {
-                mainClass = 'income';
+        public divideOperation(selectOperationId: any) {
+            this.operationId = selectOperationId;
+            this.showDivideDialog = true;
+        }
+
+        public createOperation() {
+            this.showCreateDialog = true;
+        }
+
+        public editOperation(selectOperationId: any) {
+            this.operationId = selectOperationId;
+            this.showEditDialog = true;
+        }
+
+        public shoppingListFormatter(shoppingList: any[]) {
+            if (!shoppingList) {
+                return '';
             }
-
-            if (index % 2 === 0) {
-                return mainClass + ' odd';
-            } else {
-                return mainClass + ' even';
-            }
-        },
-    },
-})
-export default class OperationsList extends Vue {
-
-    @Prop({default: []})
-    public operations!: object[];
-
-    @Prop({default: false})
-    public loading!: boolean;
-
-    public operationId = null;
-    public showEditDialog = false;
-    public showDivideDialog = false;
-
-    public deleteOperation(id: string) {
-        if (confirm('Вы действительно хотите удалить операцию?')) {
-            OperationService.delete(id)
-                .then((response) => {
-                        this.$root.$emit('operationChanged');
-                    },
-                );
+            return shoppingList.map((val) => (val.name)).toString();
         }
     }
-
-    public divideOperation(selectOperationId: any) {
-        this.operationId = selectOperationId;
-        this.showDivideDialog = true;
-    }
-
-    public editOperation(selectOperationId: any) {
-        this.operationId = selectOperationId;
-        this.showEditDialog = true;
-    }
-
-    public shoppingListFormatter(shoppingList: any[]) {
-        if (!shoppingList) {
-            return '';
-        }
-        return shoppingList.map((val) => (val.name)).toString();
-    }
-}
 </script>
 
 <style>
